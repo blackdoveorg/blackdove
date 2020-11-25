026303bf985f896444944d5a5e4fc85317a7a0f3
Black Dove <root@blackdove.org>
2020-11-25 10:51:08 -0800

    Added default compass values.

---
+++ database/migrations/2020_11_25_184810_set_default_compass_values.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SetDefaultCompassValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->decimal('social_compass')->default(0);
+            $table->decimal('economic_compass')->default(0);
+            $table->decimal('compass_color')->default('ffffff');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
