ec18e6e4aa651cb1b019e8f7b3d961960ba9c1b5
blackdoveorg <mark.ciubal@gmail.com>
2021-01-27 00:20:59 -0800

    Added ServerTiming.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        // \BeyondCode\ServerTiming\Middleware\ServerTimingMiddleware::class,
+        \BeyondCode\ServerTiming\Middleware\ServerTimingMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
